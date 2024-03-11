5bd1c392044195b41de7c96a144711e7b50d42b5
kadetete <kadetesan@gmail.com>
2024-03-11 10:18:49 -0300

    montando estrutura dos testes

---
+++ test_tarefas.py
@@ -0,0 +1,17 @@
+import unittest
+class Test_tarefas:
+        
+    def test_usuario(self):
+        pass
+
+    def test_criar_tarefas(self):
+        pass
+
+    def test_priorizar_tarefas(self):
+        pass
+
+    def test_concluir_terefa(self):
+        pass
+
+    def test_registrar_progresso(self):
+        pass
