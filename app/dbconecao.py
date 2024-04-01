7c1e36f12bb861079076450f7418bd58074dc19f
Guilherme Cadete <110258437+kadetete@users.noreply.github.com>
2024-04-01 01:06:59 +0000

    Finalizando teste de cobertura

--- app/dbconecao.py
+++ app/dbconecao.py
@@ -5,39 +5,27 @@ class ListTask:
         self.conexao = self.conectar_banco()
         if self.conexao is not None:
             self.cursor = self.conexao.cursor()
-        else:
-            print("Não foi possível conectar ao banco de dados.")
-            exit()
 
     def conectar_banco(self):
         host = 'mysql'
         usuario = 'root'
         senha = ''
-        banco = 'dblista_tarefa'
-
-        try:
-            conexao = mysql.connector.connect(
-                host=host,
-                user=usuario,
-                password=senha,
-                database=banco
-            )
-            print("Conexão bem-sucedida ao banco de dados!")
-            return conexao
-
-        except mysql.connector.Error as erro:
-            print("Erro ao conectar ao banco de dados:", erro)
-            return None
+        banco = 'dblista_tarefa' 
+        conexao = mysql.connector.connect(
+            host=host,
+            user=usuario,
+            password=senha,
+            database=banco
+        )
+        print("Conexão bem-sucedida ao banco de dados!")
+        return conexao
 
     def adicionar(self, tarefa, priori, status):
-        try:
-            sql = "INSERT INTO tarefas (nome, prioridade, status) VALUES (%s, %s, %s)"
-            val = (tarefa, priori, status)
-            self.cursor.execute(sql, val)
-            self.conexao.commit()
-            print("Tarefa adicionada com sucesso!")
-        except mysql.connector.Error as erro:
-            print("Erro ao adicionar a tarefa:", erro)
+        sql = "INSERT INTO tarefas (nome, prioridade, status) VALUES (%s, %s, %s)"
+        val = (tarefa, priori, status)
+        self.cursor.execute(sql, val)
+        self.conexao.commit()
+        print("Tarefa adicionada com sucesso!")
 
     def finalizar(self, indice_tarefa):
         try:
@@ -50,7 +38,6 @@ class ListTask:
             print("Erro ao finalizar a tarefa:", erro)
     
     def exibir(self, priori):
-        try:
             if priori == '4':
                 sql = "SELECT * FROM `tarefas` ORDER BY `tarefas`.`prioridade` ASC"
                 self.cursor.execute(sql)
@@ -66,12 +53,9 @@ class ListTask:
                     print(f"{indice + 1}. {tarefa[1]} - Prioridade: {tarefa[2]} - Status: {tarefa[3]}")
                 tarefas_formatadas = [{'idtarefa': tarefa[0], 'nome': tarefa[1], 'prioridade': tarefa[2], 'status': tarefa[3]} for tarefa in tarefas]
                 return tarefas_formatadas
-        except mysql.connector.Error as erro:
-            print("Erro ao filtrar as tarefas:", erro)
-            return []
    
     def limpar_tarefas(self):
-        try:
+        # try:
             # Remover todos os registros da tabela
             sql_delete = "DELETE FROM tarefas;"
             self.cursor.execute(sql_delete)
@@ -85,5 +69,5 @@ class ListTask:
 
 
             print("Todos os dados da tabela 'tarefas' foram apagados com sucesso!")
-        except mysql.connector.Error as erro:
-            print("Erro ao limpar os dados da tabela 'tarefas':", erro)
+        # except mysql.connector.Error as erro:
+        #     print("Erro ao limpar os dados da tabela 'tarefas':", erro)
